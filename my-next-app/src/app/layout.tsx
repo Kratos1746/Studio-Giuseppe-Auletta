cfc591293a0003df7e352db64050a67c0661c3f5
Gaetano Mosca <gaetano.mosca13@gmail.com>
2025-04-29 13:05:11 +0200

    main

--- my-next-app/src/app/layout.tsx
+++ my-next-app/src/app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
     <html lang="it">
       <Analytics/>
       <body className="flex flex-col min-h-screen mx-auto text-sm  xl:text-base">     
-        <main>{children}</main>
+        <main className='bg-gray-200'>{children}</main>
         <Footer />
       </body>
     </html>
